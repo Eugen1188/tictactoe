8dcb8caecc7a7c3ff7ea94de687108bb0d5f2372
Eugen Ferchow <Eugen.Ferchow@gmx.de>
2022-03-04 17:03:14 +0100

    completed

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
 let fields = [];
-
+let gameOver = false;
 let currentShape = 'cross';
+let AUDIO_START = new Audio('audio/play.mp3');
+let AUDIO_PLAY = new Audio('audio/swoosh.mp3');
+let AUDIO_GAMEOVER= new Audio('audio/gameover.mp3')
+
+function playGame(){
+    AUDIO_START.play();
+    document.getElementById('intro').classList.add('d-none');
+    document.getElementById('main-container').classList.remove('d-none');
+}
 
 function fillShape(id) {
-    if (!fields[id]) {
+    if (!fields[id] && !gameOver) {
         if (currentShape == 'cross') {
+            AUDIO_PLAY.play();
             currentShape = 'circle';
             document.getElementById('player-2').classList.remove('player-inactive');
             document.getElementById('player-1').classList.add('player-inactive');
         } else {
+            AUDIO_PLAY.play();
             currentShape = 'cross';
             document.getElementById('player-2').classList.add('player-inactive');
             document.getElementById('player-1').classList.remove('player-inactive');
         }
+        fields[id] = currentShape;
+        console.log(fields);
+        draw();
+        checkForWin();
     }
+}
 
-    fields[id] = currentShape;
-    console.log(fields);
-    draw();
-    checkForWin();
+function restart(){
+    gameOver = false;
+    fields = [];
+    document.getElementById('game-over').classList.add('d-none');
+    document.getElementById('restart-btn').classList.add('d-none');
+
+    for(i=1; i < 9; i++){
+        document.getElementById(`line-${i}`).style.transform = 'scaleX(0)';
+    }
+    for(i=0; i < 9; i++){
+        document.getElementById(`circle-${i}`).classList.add('d-none');
+        document.getElementById(`cross-${i}`).classList.add('d-none');
+    }
 }
 
 function draw() {
@@ -26,7 +51,6 @@ function draw() {
         if (fields[i] == 'circle') {
             document.getElementById('circle-' + i).classList.remove('d-none');
         }
-
         if (fields[i] == 'cross') {
             document.getElementById('cross-' + i).classList.remove('d-none');
         }
@@ -35,33 +59,50 @@ function draw() {
 
 function checkForWin() {
     let winner;
-
     if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         winner = fields[0];
+        document.getElementById('line-1').style.transform = 'scaleX(1)';
     }
     if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
         winner = fields[3];
+        document.getElementById('line-2').style.transform = 'scaleX(1)';
     }
     if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
         winner = fields[6];
+        document.getElementById('line-3').style.transform = 'scaleX(1)';
     }
     if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
         winner = fields[0];
+        document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1)';
     }
     if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
         winner = fields[1];
+        document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1)';
     }
     if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
         winner = fields[2];
+        document.getElementById('line-6').style.transform = 'rotate(90deg) scaleX(1)';
     }
     if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         winner = fields[0];
+        document.getElementById('line-7').style.transform = 'rotate(45deg) scaleX(1.32)';
     }
     if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
         winner = fields[2];
+        document.getElementById('line-8').style.transform = 'rotate(-45deg) scaleX(1.32)';
     }
-    if (winner) {
-        console.log('Gewonnen:', winner)
-    }
+    determineWinner(winner);
+}
 
+function determineWinner(winner){
+    if (winner) {
+        console.log('Gewonnen:', winner);
+        gameOver = true;
+
+        setTimeout(function () {
+            document.getElementById('game-over').classList.remove('d-none');
+            document.getElementById('restart-btn').classList.remove('d-none');
+            AUDIO_GAMEOVER.play();
+        }, 500);
+    }
 }
